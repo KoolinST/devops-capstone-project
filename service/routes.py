78e910e447ba0fea70816ce0f748fddf60a216c1
KoolinST <LargeEPS@gmail.com>
2025-03-31 20:10:24 -0400

    Added the ability to update an account from the service

--- service/routes.py
+++ service/routes.py
@@ -86,6 +86,7 @@ def read_account(id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:id>", methods=["PUT"])
 def update_account(id):
     """
@@ -97,7 +98,7 @@ def update_account(id):
     account = Account.find(id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{id}] could not be found.")
-    
+
     account.deserialize(request.get_json())
     account.update()
 
